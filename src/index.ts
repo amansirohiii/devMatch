871b32652fbe6c7e34c14962e0816b5b13d12a7d
Aman Sirohi <amansirohi077@gmail.com>
2024-10-22 23:29:37 +0530

    added prod cors origin

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:3000", "https://dev-match-web.vercel.app"],
     credentials: true,
 }));
 app.use(
