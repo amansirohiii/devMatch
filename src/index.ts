5c0620a9cfe5ea0aec97ed92a9f8b950c8657376
Aman Sirohi <amansirohi077@gmail.com>
2024-10-23 13:13:24 +0530

    added sameSite condition

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ app.use(
             secure: process.env.NODE_ENV === "production",
             httpOnly: true,
             maxAge: 1000 * 60 * 60 * 24,
-            sameSite: "none",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
           },
     })
 );
